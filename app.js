fb2396b8546f72b56874b0ce932302217445e94c
SwethaKalimela <kalimelaswetha@gmail.com>
2018-07-04 22:09:23 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const employeeRoute = require('./routes/employees');
 const protfolioviewRoute = require('./routes/protfolioview');
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //configure this middleware to use ejs template engine in the app
 
-app.use('/employees', employeeRoute);
-app.use('/protfolioview', protfolioviewRoute); 
+app.use('/employees', employeeRoute); //configure the employee middleware to define /employee route
+app.use('/protfolioview', protfolioviewRoute); //configure the protfolioview middleware to define /protfolioview route
 
 module.exports = app;
