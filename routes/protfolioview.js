c042de22dfffc6cad108bff99271e6543c8b0e21
SwethaKalimela <kalimelaswetha@gmail.com>
2018-07-04 22:06:01 +0000

    Update protfolioview.js

--- routes/protfolioview.js
+++ routes/protfolioview.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 
-let payroll = fs.readFileSync('./payroll.json');
+let payroll = fs.readFileSync('./payroll.json'); //reading the JSON file using fs to payroll object
 let emppayroll = JSON.parse(payroll);
 var employee = [" "];
 var i = 0;
 
-router.get('/:name', (req,res,next) => {
+router.get('/:name', (req,res,next) => { //generate view for a particular employee based on his name in params, Using ejs template engine
     const name = req.params.name;
     for (p of emppayroll) {
     if(p.name == name ){       
-        res.render('protfolioview',{data:p});
+        res.render('protfolioview',{data:p}); //passing that particular employees record whose name matches with original json object to protfolioview.ejs
         
     } else if (i < emppayroll.length-1) { 
         i++;continue;
